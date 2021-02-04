5807d5fc1f90d00ecbd1930aecd991c237978068
Thibault Verin <thibaultverin@hotmail.com>
2021-02-04 22:15:39 +0100

    Modal with style done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.scss';
+import Modal from './HomeModal';
+import './HomeModal.scss';
 
 const Home = () => {
-  return <div>Hello Bitch</div>;
-};
+  const isModalOpen = true;
 
+  return (
+    <div className='home-page'>
+      <Modal showModal={isModalOpen}>
+        <div className='modalHeaderHome'>
+          <h2>Hold on ...</h2>
+        </div>
+        <div className='modalBody'>
+          <h3>
+            You need to be over 18 to access this website
+            <br />
+          </h3>
+        </div>
+        <div className='modalFooterHome'>
+          <button type='button' className='modalButton'>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.alcool-info-service.fr/?gclid=Cj0KCQiA0-6ABhDMARIsAFVdQv-qYqyfeaniSATWeQcUJXUE3qx2I0MmGOweodhxizo8KP_FdlMd6cAaAhdNEALw_wcB'
+            >
+              I am under 18
+            </a>
+          </button>
+          <Link to='/beer-list'>
+            {' '}
+            <button type='button' className='modalButton'>
+              I am over 18
+            </button>
+          </Link>
+        </div>
+      </Modal>
+      <div className='logo-home'></div>
+      <div className='title-home'>Wiki Qui Mousse</div>
+    </div>
+  );
+};
 export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.scss';
-
 const Navbar = () => {
-  return <div className='navbar-container'>Hello Bitch</div>;
+  return (
+    <div className='navbar-container'>
+      <Link to='/beer-list'>
+        <div className='beer-list' />
+      </Link>
+      <Link to='/post-beer'>
+        <div className='post-beer' />
+      </Link>
+    </div>
+  );
 };
-
 export default Navbar;
