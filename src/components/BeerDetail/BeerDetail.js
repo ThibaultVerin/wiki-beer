29ffb947503b648d05dd73a4e44448795c0cfddd
Thibault Verin <thibaultverin@hotmail.com>
2021-02-05 00:58:16 +0100

    beer detail done with CSS and responsive

--- src/components/BeerDetail/BeerDetail.js
+++ src/components/BeerDetail/BeerDetail.js
@@ -1,6 +1,8 @@
 import react, { useState, useEffect } from 'react';
 import './BeerDetail.scss';
 import axios from 'axios';
+import heart from '../Picture/heart.svg';
+import { Link } from 'react-router-dom';
 
 const BeerDetail = (props) => {
   const { match } = props;
@@ -18,11 +20,29 @@ const BeerDetail = (props) => {
   return (
     <>
       {beer && (
-        <div>
+        <div className='beer-detail-container'>
+          <h1>{beer.name}</h1>
           <img src={beer.image_url} alt={beer.name} />
-          <p>{beer.name}</p>
-          <div>
+          <div className='beer-detail-info'>
+            <p>Brand : {beer.brand}</p>
+            <p>Type : {beer.type}</p>
+            <p>Country : {beer.country}</p>
+            <p>Price : ${beer.price}</p>
+          </div>
+          <div className='beer-detail-description'>
+            <h2>Description</h2>
             <p>{beer.description}</p>
+          </div>
+          <div className='beer-detail-advice'>
+            <h2>Advice</h2>
+            <p>{beer.advice}</p>
+          </div>
+          <div className='beer-detail-button'>
+            <img src={heart} alt='favorite' />
+            <Link to='/beer-list'>
+              {' '}
+              <button type='button'>Back</button>
+            </Link>
           </div>
         </div>
       )}
